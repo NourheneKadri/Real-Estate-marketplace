dfa9bda0dbc6e811bca057eb7a9daedbb9015034
Nourhen kadri <kadnourhen@gmail.com>
2024-10-09 21:19:35 +0100

    First commit (Project)

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At our Immobilier Marketplace, we connect individuals and businesses
+        with their ideal real estate solutions, whether they're seeking to buy,
+        sell, or rent properties. Our platform is designed to streamline the
+        process, offering a comprehensive range of listings and tools to
+        facilitate smooth transactions.
+      </p>
+
+      <p className="mb-4 text-slate-700 font-bold">Who We Are</p>
+      <p className="mb-4 text-slate-700">
+        We are a team of passionate professionals dedicated to revolutionizing
+        the way people interact with real estate. With a wealth of experience in
+        both technology and the property market, we've created a platform that
+        combines cutting-edge innovation with user-friendly design.
+      </p>
+
+      <p className="mb-4 text-slate-700 font-bold">What We Offer</p>
+
+      <p className="mb-4 text-slate-700">
+        <ul>
+          <li>
+            Property Listings: Explore a diverse range of properties including
+            residential homes, commercial spaces, land plots, and more.
+          </li>
+          <li>
+            Advanced Search Filters: Narrow down your search with precision
+            using our advanced search filters.
+          </li>
+          <li>
+            Sell Your Property: List your property and gain exposure to a wide
+            audience of potential buyers.
+          </li>
+          <li>
+            Rent Properties: Browse through our extensive collection of rental
+            listings.
+          </li>
+          <li>
+            Professional Services: Get assistance with legal matters, financing,
+            or property management.
+          </li>
+        </ul>
+      </p>
+
+      <p className="mb-4 text-slate-700 font-bold">Why Choose Us</p>
+      <p className="mb-4 text-slate-700">
+        <ul>
+          <li>
+            User-Friendly Interface: Our platform is designed with the user in
+            mind.
+          </li>
+          <li>
+            Comprehensive Listings: Find a wide range of properties to choose
+            from.
+          </li>
+          <li>
+            Transparent Transactions: We prioritize transparency and honesty in
+            all our dealings.
+          </li>
+          <li>
+            Customer Support: Our dedicated customer support team is here to
+            help.
+          </li>
+        </ul>
+      </p>
+
+      <p className="mb-4 text-slate-700 font-bold">Get Started Today</p>
+      <p className="mb-4 text-slate-700">
+        Join our Immobilier Marketplace today and unlock a world of
+        possibilities in the real estate market. Whether you're looking to buy,
+        sell, or rent, we're here to help you navigate every step of the
+        journey. Start exploring now and turn your property dreams into reality!
+      </p>
+    </div>
+  );
+}
